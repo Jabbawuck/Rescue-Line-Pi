b77528e70bd221b7d889c103a0889f6c050c5f91
LaezGame <67858743+LaezGame@users.noreply.github.com>
2023-12-04 18:46:50 +0100

    Converted Arduino files to sketchfolder
    Added include and first placeholder pin assignments

---
+++ ArduinoMain/arduinoMain.ino
@@ -0,0 +1,1 @@
+#include arduino_dependencies.ino
---
+++ ArduinoMain/pindef.ino
@@ -0,0 +1,15 @@
+//[Pin Definition]
+//[Motors]
+int motorLPin1 = ;
+int motorLPin2 = ;
+unsigned char motorLSpeedPin = ;
+
+int motorRPin1 = ;
+int motorRPin2 = ;
+unsigned char motorRSpeedPin = ;
+
+//[Servos]
+
+//[Gyrosensor]
+
+//[]
