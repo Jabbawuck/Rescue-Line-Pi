10eb6fd29c916de4b96329dd0a8605cfb1db4440
justyourbasicjan <68247438+justyourbasicjan@users.noreply.github.com>
2024-02-06 21:33:01 +0100

    Update to lineCam.py-> added camera preview?
    bin mir nicht sicher, ob das richtig ist, naja

---
+++ lineCam.py
@@ -0,0 +1,23 @@
+from picamera import PiCamera
+from time import sleep
+import cv2
+import numpy as np
+camera = PiCamera()
+
+
+#camera test
+camera.start_preview()
+sleep(5)
+"""camera.capture('/home/pi/Desktop/image.jpg')# takepicture; saved on desktop"""
+camera.stop_preview()
+
+camera.start_preview(alpha=200)# durchlässigkeit vom preview ; 0 bis 255
+
+# videorecording
+"""camera.start_preview()
+camera.start_recording('/home/pi/Desktop/video.h264')
+sleep(5)
+camera.stop_recording()
+camera.stop_preview()"""
+
+
