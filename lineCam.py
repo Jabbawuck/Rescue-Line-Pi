15b08808efe110d63fbc36313e346101e73d54bd
LaezGame <67858743+LaezGame@users.noreply.github.com>
2024-02-17 23:30:06 +0100

    Working camera Modul

--- lineCam.py
+++ lineCam.py
@@ -1,23 +1,20 @@
-from picamera2 import PiCamera2
-from time import sleep
 import cv2
-import numpy as np
-camera = PiCamera()
+from picamera2 import Picamera2
 
 
-#camera test
-camera.start_preview()
-sleep(5)
-"""camera.capture('/home/pi/Desktop/image.jpg')# takepicture; saved on desktop"""
-camera.stop_preview()
+thres = 0.45 # Threshold to detect object
 
-camera.start_preview(alpha=200)# durchlässigkeit vom preview ; 0 bis 255
+picam = Picamera2()
 
-# videorecording
-"""camera.start_preview()
-camera.start_recording('/home/pi/Desktop/video.h264')
-sleep(5)
-camera.stop_recording()
-camera.stop_preview()"""
+if __name__ == "__main__":
 
+
+    picam.start()
+
+    while True:
+        picam.capture_file("videostream.jpg")
+        img = cv2.imread("videostream.jpg", -1)
+        
+        cv2.imshow("Output",img)
+        cv2.waitKey(1)
 
