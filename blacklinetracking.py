9a24a225d318d4f8804aaf62b1827ec33d982131
Jan Nguyen <jannguyen2006@gmail.com>
2024-02-17 10:43:39 +0100

    try reading blacklines and first part videocamera test
    gitpush

--- blacklinetracking.py
+++ blacklinetracking.py
@@ -1,5 +1,13 @@
 import cv2
 import numpy as np
+from picamera import PiCamera
+from time import sleep
+
+camera.start_preview()
+camera.start_recording('/home/pi/Desktop/video.h264')
+sleep(5)
+camera.stop_recording()
+camera.stop_preview()
 
 def track_black_line():
    
