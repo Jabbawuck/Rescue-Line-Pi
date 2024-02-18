5a73adaf42a62bf14c498b396623009a7e6054a1
LaezGame <67858743+LaezGame@users.noreply.github.com>
2024-02-18 23:13:22 +0100

    Add files via upload

--- lineCam.py
+++ lineCam.py
@@ -1,7 +1,6 @@
 import cv2
 from picamera2 import Picamera2
 import numpy as np
-import utlis
 
 thres = 0.45 # Threshold to detect object
 
@@ -16,7 +15,7 @@ def track_line(image):
     
     _, thresholded = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)
 
-    contours, _ = cv2.findContours(thresholded, cv2.RETR_External, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     for contour in contours:
         area = cv2.contourArea(contour)
@@ -32,7 +31,7 @@ def track_line(image):
             cv2.circle(image, (center_x, center_y), 5, (0, 0, 255), -1)
             
             image_center_x, _ = image.shape[1] // 2, image.shape[0] // 2
-            if center_x < image_center_x:
+            if center_x < imlage_center_x:
                 print("Line is on the left")
             elif center_x > image_center_x:
                 print("Line is on the right")
@@ -42,11 +41,21 @@ def track_line(image):
     return image
     
 imgBgr = cv2.imread('videostream.jpg')
+
+
+    
+def thresholding(img):
+    imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([87, 87, 87])
+    maskBlack = cv2.inRange(imgHsv,lower_black,upper_black)
+    return maskBlack
     
     
 def getLaneCurve(img):
 
-    imgThres = utlis.thresholding(img)
+    imgThres = thresholding(img)
 
     cv2.imshow('Thres',imgThres)
     return None
@@ -65,8 +74,8 @@ if __name__ == "__main__":
         
 		cv2.imshow("Output",img)
 		cv2.waitKey(1)
-'''    
-	while True:
+    
+	
 		ret, frame = cap.read()
 		if not ret:
 			break
@@ -74,18 +83,21 @@ if __name__ == "__main__":
         
 		result =track_line(frame)
 		cv2.imshow('Line Tracking', result)
+		cv2.waitKey(1)
+		
+		
     
-		if cv2.waitKey(1) & OxFF == ord('q'):
-			break
-        '''
+		#if cv2.waitKey(1) & OxFF == ord('q'):
+			#break
+        
         
 
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 
 
 
 
-cap.release()
-cv2.destroyAllWindows()
+#cap.release()
+#cv2.destroyAllWindows()
 
